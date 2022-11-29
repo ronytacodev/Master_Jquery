1091541bba3f1eb4c6ef5cab0241428dd7cf9b0e
RonyTCdev <64666995+ronyfringe@users.noreply.github.com>
2022-11-29 09:48:11 -0500

    mas jquery

--- curso_jquery_bluuweb/app.js
+++ curso_jquery_bluuweb/app.js
@@ -1,11 +1,20 @@
 $(document).ready(function(){
 
     // console.log('functionando');
-    $('h1').html('Etiqueta h1');
-    $('.display-4').html('desde clase');
-    $('#idh1').html('desde id');
+    // $('h1').html('Etiqueta h1');
+    // $('.display-4').html('desde clase');
+    // $('#idh1').html('desde id');
 
-    document.querySelector('h1').innerHTML = 'Etiqueta 1';
-    document.querySelector('.display-4').innerHTML = 'desde clase';
-    document.querySelector('#idh1').innerHTML = 'desde id';
+    // document.querySelector('h1').innerHTML = 'Etiqueta 1';
+    // document.querySelector('.display-4').innerHTML = 'desde clase';
+    // document.querySelector('#idh1').innerHTML = 'desde id';
+
+    // $('.container h1').html('cambio de texto');
+    // $('.container h1:first').html('cambio de texto');
+    // $('.container h1:last').html('cambio de texto');
+
+    $('#idh1').addClass('text-danger');
+    $('#idh1').removeClass('display-4');
+
+    // me quede en el min 20:25
 });
