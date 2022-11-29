30c73a22ef7482a56517ce65cfd11606b8b33ba1
RonyTCdev <64666995+ronyfringe@users.noreply.github.com>
2022-11-29 09:39:11 -0500

    selectores

--- curso_jquery_bluuweb/app.js
+++ curso_jquery_bluuweb/app.js
@@ -1,4 +1,11 @@
 $(document).ready(function(){
 
     // console.log('functionando');
+    $('h1').html('Etiqueta h1');
+    $('.display-4').html('desde clase');
+    $('#idh1').html('desde id');
+
+    document.querySelector('h1').innerHTML = 'Etiqueta 1';
+    document.querySelector('.display-4').innerHTML = 'desde clase';
+    document.querySelector('#idh1').innerHTML = 'desde id';
 });
