90100da0284b0101c587ae7dd8b011e841d4cbde
RonyTCdev <64666995+ronyfringe@users.noreply.github.com>
2022-12-01 09:42:57 -0500

    ajax, funcion get con json

--- ajax_jquery/app.js
+++ ajax_jquery/app.js
@@ -5,8 +5,21 @@ $(document).ready(function () {
         // console.log("click en boton");
         $.get("archivo.txt", function (data, textStatus, jqXHR) {
             console.log(data);
-            console.log(textStatus);
-            console.log(jqXHR);
+            // console.log(textStatus);
+            // console.log(jqXHR);
           });
     });
+
+    $('#leerEmpleado').click(function (e) { 
+        e.preventDefault();
+        $.get("empleados.json", function(data) {
+            console.log(data);
+            $('#datosEmpleado').html(`
+            Nombre: ${data.nombre} <br>
+            Puesto: ${data.puesto} <br>
+            Edad: ${data.edad} <br>
+            `);
+        });
+    });
+
 });
