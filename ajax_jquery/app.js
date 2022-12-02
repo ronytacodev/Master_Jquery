05044891c39fe1a4f79cefcd7cb15e2f40436e4f
RonyTCdev <64666995+ronyfringe@users.noreply.github.com>
2022-12-02 09:48:03 -0500

    avance de ajax

--- ajax_jquery/app.js
+++ ajax_jquery/app.js
@@ -1,77 +1,90 @@
 // console.log("hola");
 $(document).ready(function () {
-    $('#leer').click(function (e) { 
-        e.preventDefault();
-        // console.log("click en boton");
-        $.get("archivo.txt", function (data, textStatus, jqXHR) {
+    // $('#leer').click(function (e) { 
+    //     e.preventDefault();
+    //     // console.log("click en boton");
+    //     $.get("archivo.txt", function (data, textStatus, jqXHR) {
+    //         console.log(data);
+    //         // console.log(textStatus);
+    //         // console.log(jqXHR);
+    //       });
+    // });
+
+    // $('#leerEmpleado').click(function (e) { 
+    //     e.preventDefault();
+    //     $.get("empleados.json", function(data) {
+    //         console.log(data);
+    //         $('#datosEmpleado').html(`
+    //         Nombre: ${data.nombre} <br>
+    //         Puesto: ${data.puesto} <br>
+    //         Edad: ${data.edad} <br>
+    //         `);
+    //     });
+    // });
+
+    // $('#leerArregloEmpleado').click(function (e) { 
+    //     e.preventDefault();
+    //     $('#listaEmpleados').html('');
+    //     $.get("empleados.json",
+    //         function (data) {
+    //             // console.log(data);
+    //             $.each(data, function (index, item) { 
+    //                  $('#listaEmpleados').html($('#listaEmpleados').html()+ `
+    //                     <li>${item.nombre} -- ${item.puesto} </li>
+    //                     `);
+    //             });
+    //         },
+    //     );
+    // });
+
+    // $('#leerGetJSON').click(function (e) { 
+    //     e.preventDefault();
+
+    //     // $.get("empleados.txt", function(data) {
+    //     //     data= JSON.parse(data);
+    //     //     console.log(data);
+    //     // });
+
+    //     $.getJSON("empleados.json", function(data) {
+    //         console.log(data.empleados);
+    //         $('#listaEmpleados').html('');
+    //         $.each(data.temporales, function (index, item) { 
+    //             $('#listaEmpleados').html($('#listaEmpleados').html()+ `
+    //                <li>${item.nombre} -- ${item.puesto} </li>
+    //                `);
+    //        });
+    //     });
+
+    // });
+
+
+    // let empleados;
+    // $.getJSON("empleados.json", function(data) {
+    //     empleados = data.empleados;
+    // });
+    // $('#nombre').keyup(function (e) { 
+    //     $('#listaEmpleados').html('');
+    //     let nombre = $(this).val();
+    //     $.each(empleados, function (indexInArray, item) { 
+    //          if(item.nombre.toLowerCase().indexOf(nombre.toLowerCase())!==-1){
+    //             $('#listaEmpleados').html($('#listaEmpleados').html()+ `
+    //                <li>${item.nombre} -- ${item.puesto} </li>
+    //                `);
+    //          }
+    //     });
+    // });
+
+
+   // ajax  //
+   $('#ajax').click(function (e) { 
+    e.preventDefault();
+    $.ajax({
+        url: 'empleados.json',
+        type: 'get',
+        dataType: 'json',
+        success: function(data) {
             console.log(data);
-            // console.log(textStatus);
-            // console.log(jqXHR);
-          });
-    });
-
-    $('#leerEmpleado').click(function (e) { 
-        e.preventDefault();
-        $.get("empleados.json", function(data) {
-            console.log(data);
-            $('#datosEmpleado').html(`
-            Nombre: ${data.nombre} <br>
-            Puesto: ${data.puesto} <br>
-            Edad: ${data.edad} <br>
-            `);
-        });
-    });
-
-    $('#leerArregloEmpleado').click(function (e) { 
-        e.preventDefault();
-        $('#listaEmpleados').html('');
-        $.get("empleados.json",
-            function (data) {
-                // console.log(data);
-                $.each(data, function (index, item) { 
-                     $('#listaEmpleados').html($('#listaEmpleados').html()+ `
-                        <li>${item.nombre} -- ${item.puesto} </li>
-                        `);
-                });
-            },
-        );
-    });
-
-    $('#leerGetJSON').click(function (e) { 
-        e.preventDefault();
-
-        // $.get("empleados.txt", function(data) {
-        //     data= JSON.parse(data);
-        //     console.log(data);
-        // });
-
-        $.getJSON("empleados.json", function(data) {
-            console.log(data.empleados);
-            $('#listaEmpleados').html('');
-            $.each(data.temporales, function (index, item) { 
-                $('#listaEmpleados').html($('#listaEmpleados').html()+ `
-                   <li>${item.nombre} -- ${item.puesto} </li>
-                   `);
-           });
-        });
-
-    });
-
-
-    let empleados;
-    $.getJSON("empleados.json", function(data) {
-        empleados = data.empleados;
-    });
-    $('#nombre').keyup(function (e) { 
-        $('#listaEmpleados').html('');
-        let nombre = $(this).val();
-        $.each(empleados, function (indexInArray, item) { 
-             if(item.nombre.toLowerCase().indexOf(nombre.toLowerCase())!==-1){
-                $('#listaEmpleados').html($('#listaEmpleados').html()+ `
-                   <li>${item.nombre} -- ${item.puesto} </li>
-                   `);
-             }
-        });
-    });
-
+        }
+    })
+   });
 });
