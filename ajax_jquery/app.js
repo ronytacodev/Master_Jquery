a487b64b2cd39506013bf559517a410273fc67fc
RonyTCdev <64666995+ronyfringe@users.noreply.github.com>
2022-12-02 20:30:29 -0500

    mas ajax

--- ajax_jquery/app.js
+++ ajax_jquery/app.js
@@ -78,12 +78,23 @@ $(document).ready(function () {
    // ajax  //
    $('#ajax').click(function (e) { 
     e.preventDefault();
+    $('#listaEmpleados').html('');
     $.ajax({
         url: 'empleados.json',
         type: 'get',
         dataType: 'json',
         success: function(data) {
             console.log(data);
+            $.each(data.empleados, function (i, item) { 
+                 $('#listaEmpleados').html($('#listaEmpleados').html()+`
+                 <li> ${item.nombre} </li>
+                 `);
+            });
+        },
+        error:function(xhr, status, error){
+            console.log(xhr);
+            console.log(status);
+            console.log(error);
         }
     })
    });
