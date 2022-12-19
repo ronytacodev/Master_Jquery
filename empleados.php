34437c650c971b97607c8a3b827e17065b779662
RonyTCdev <64666995+ronyfringe@users.noreply.github.com>
2022-12-19 11:31:30 -0500

    código php para crud -crear

--- empleados.php
+++ empleados.php
@@ -4,19 +4,28 @@
     $usuario="root";
     $password="";
     $bd="empleados";
-    $mysql=new mysqli($servidor,$usuario,$password,$bd);
+    $mysqli=new mysqli($servidor,$usuario,$password,$bd);
+    if($_REQUEST['accion']=="leer"){
+        $arreglo=array();
 
-    $arreglo=array();
+        $consulta="SELECT id,nombre,puesto,edad FROM empleados";
 
-    $consulta="SELECT id,nombre,puesto,edad FROM empleados";
-
-    if($resultado=$mysql->query($consulta)){
-        while($row = $resultado->fetch_array(MYSQLI_ASSOC)){
-            $arreglo[]=$row;
+        if($resultado=$mysql->query($consulta)){
+            while($row = $resultado->fetch_array(MYSQLI_ASSOC)){
+                $arreglo[]=$row;
+            }
+            echo json_encode($arreglo);
         }
-        echo json_encode($arreglo);
+        $resultado->close();
     }
-    $resultado->close();
+    else if($_REQUEST['accion']=="insertar") {
+        $sql="INSERT into empleados (nombre,puesto,edad) values ('".$_POST["nombre"]."', '".$_POST["puesto"]."' , '".$_POST["edad"]."')";
+        if($mysqli->query($sql) === TRUE){
+            echo "1";
+        }else {
+            echo "0";
+        }
+    }
     $mysql->close();
 
 ?>
