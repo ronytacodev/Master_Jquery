cd1ef5994b46fc3f2711f434228cafbc05e4a023
RonyTCdev <64666995+ronyfringe@users.noreply.github.com>
2022-12-19 10:26:44 -0500

    crud leer registros de tabla

---
+++ ajax_jquery/crud.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    $('#leerEmpleados').click(function (e) { 
+        e.preventDefault();
+        $('#listaEmpleados').html('');
+        $.getJSON("http://localhost/ajax_jquery/jquery_ajax/empleados.php",
+            function (data) {
+                // console.log(data);
+                $.each(data, function (index, item) { 
+                     $('#listaEmpleados').html($('#listaEmpleados').html()+`
+                     <li>${item.nombre} -- ${item.puesto} -- ${item.edad}</li>
+                     `);
+                });
+            }
+        );
+    });
+});
